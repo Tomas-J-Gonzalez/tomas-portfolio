af32758038b93a2fb47c902ab3fbcc15943a080d
Tomás González <tomachos08@gmail.com>
2024-03-08 17:03:54 +1300

    Using handlebar.jr to create the SiteHead module Adding js script to auto underline active navpage

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,13 +1,3 @@
-const cl = cloudinary.Cloudinary.new({ cloud_name: "tomasgo" });
-cl.responsive();
-
-$(document).ready(function() {
-  $("img").lazyload({
-    threshold: 200,
-    effect: "fadeIn"
-  });
-});
-
 //slow scroll to top of page
 
 $(document).ready(function() {
@@ -25,3 +15,57 @@ $(document).ready(function() {
 });
 
 //slow scroll to top of page --end
+
+function announceRouteChange(message) {
+    var routeAnnouncer = document.getElementById("routeAnnouncer");
+    routeAnnouncer.textContent = message;
+}
+
+// Handlebar.js for Navigation module
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Data object for SiteHead__template
+    const data = {
+        pages: [
+            { title: 'Index', url: '/', id: 'index-link' },
+            { title: 'Bio', url: '/bio.html', id: 'bio-link' },
+            { title: 'Writing', url: '/writing.html', id: 'writing-link' },
+        ]
+    };
+
+    // Handlebars navigation template (Declared within HTML File)
+    const templateSource = document.getElementById('SiteHead__template').innerHTML;
+    const template = Handlebars.compile(templateSource);
+
+    // Render the template with the data
+    const renderedHtml = template(data);
+
+    // Insert the rendered HTML into the SiteHead container (Exists within HTML File)
+    document.getElementById('SiteHead__container').innerHTML = renderedHtml;
+});
+
+/* Automatically add Nav--listItemLinkActive class to Active Page link  (Only for Primary NavLinks)*/
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current URL path
+    const path = window.location.pathname;
+
+    // Get the navigation links
+    const navLinks = document.querySelectorAll('.Nav--listItemLink');
+
+   // Loop through each navigation link
+    navLinks.forEach(function(link) {
+        // Get the link's href attribute
+        const href = link.getAttribute('href');
+
+        // Normalize the paths for comparison (remove leading/trailing slashes)
+        const normalizedPath = path.replace(/^\/|\/$/g, '');
+        const normalizedHref = href.replace(/^\/|\/$/g, '');
+
+        // Check if the normalized href matches the normalized current path
+        if (normalizedPath === normalizedHref) {
+            // Add the active class to the link's parent element (list item)
+            link.parentNode.classList.add('Nav--linkActive');
+        }
+    });
+});
+
